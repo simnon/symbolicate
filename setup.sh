f208ce9568d6d8b2d1c62ace428879a33893d623
Simon Coulter <simon.coulter@synchronoss.com>
2017-01-12 13:27:00 +0000

    add scripts for symbolicating files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# To have these files accessible from any folder in your terminal
+echo "sudo is needed to move files into /user/local/bin"
+sudo chmod +x symbolicate
+sudo chmod +x symbolicateFrameworks
+sudo mv symbolicate /usr/local/bin/symbolicate
+sudo mv symbolicateFrameworks /usr/local/bin/symbolicateFrameworks
